3bc2868cab22a238f132d1554870580681eb8187
Syed Hussain Ali Razvi <116099814+Hussainalirazv@users.noreply.github.com>
2025-07-05 15:15:37 +0530

    Update RentForm.js

--- frontend/src/components/RentForm.js
+++ frontend/src/components/RentForm.js
@@ -52,7 +52,7 @@ const RentForm = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await axios.post('https://hussainalirazv.pythonanywhere.com/', formData);
+      const res = await axios.post('http://localhost:5000/predict', formData);
       setPrediction(res.data.predicted_rent);
     } catch (error) {
       console.error('Prediction failed:', error);
